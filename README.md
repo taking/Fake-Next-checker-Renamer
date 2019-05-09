a02be7e46218feb8a1a0b820ce0a68c6e00073ef
Taking <ceo@taking.kr>
2019-05-09 23:56:16 +0900

    Updated 2019-05-09

---
+++ README.md
@@ -0,0 +1,35 @@
+# Fake NEXT Checker & Renamer
+
+* 간단하게 만든 스크립트입니다. 
+* Python 으로 제작되었습니다.
+
+
+
+## How-to
+
+- python ./check_fake.py /Downloads/
+- 하위 폴더의 -NEXT.mp4 이름을 recursive 하여 ffprobe 를 이용하여 체크합니다.
+- NEXT 릴에는 MH ENCODER 문구가 포함되는데 이를 확인하여 체크를 진행합니다.
+- ![](https://i.imgur.com/Nqk34FT.png)
+- NEXT 릴이 아닌데, 파일명에 -NEXT 문구가 포함이 되어 있다면 -NEXT 문구를 제거합니다.
+
+
+
+## Video
+
+[![Video Label](https://img.youtube.com/vi/zww4gcSC5oQ/maxresdefault.jpg)](https://youtu.be/zww4gcSC5oQ?t=0s)
+
+
+
+![](https://i.imgur.com/CY61c4i.png)
+
+
+
+Fake 로 체크가 되면, 파일 명에서 -NEXT는 제거됩니다.
+
+
+
+
+
+
+마지막 업데이트 : 2019-05-09 (목) 
---
+++ check_fake.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from subprocess import check_output
+import re, os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dir", help="target directory. ex) ./fake_check /Downloads/", default="") # 경로 마지막에 / 로 꼭 닫아주세요.
+
+args = parser.parse_args()
+
+PATH = args.dir
+
+print("Check Library Path:", PATH)
+notfake=[]
+fake=[]
+print("finding and renaming started...")
+for p,w,f in os.walk(PATH):
+	for file_name in f:
+		if file_name[-9:]=='-NEXT.mp4':
+			print('processing %s' %os.path.join(p,file_name))
+			path = os.path.join(p,file_name)
+			a = str(check_output('ffprobe -i "'+path+'" 2>&1 | grep "encoder"', shell=True))
+			a = a.split(',')[0].split("encoder")[1].strip()
+			if(a[2] == "M"):
+				print("NOT FAKE")
+				notfake.append(file_name)	# NOT FAKE NEXT
+			else:
+				print("FAKE")
+				fake.append(file_name) 		# FAKE NEXT
+				rename = re.sub('-NEXT', '', file_name)
+				os.rename(os.path.join(p,file_name), os.path.join(p,rename))
+
+check_list = list(set(notfake) | set(fake))
+print('\nfound NEXT files:')
+for check in check_list:
+	print(check)
+
+print('\n(found FAKE) renamed files:')
+for check in fake:
+	print(check)
